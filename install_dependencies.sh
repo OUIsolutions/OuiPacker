b7e05fe7bac5dada93ecb15536e17357c297c0c5
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-12-06 19:33:20 -0300

    at

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,7 +5,6 @@ mkdir types
 git clone -b v0.71 https://github.com/OUIsolutions/LuaDoTheWorld.git
 git clone -b V0.003 https://github.com/SamuelHenriqueDeMoraisVitrio/candangoEngine.git
 
-
-curl -L https://raw.githubusercontent.com/OUIsolutions/Darwin/refs/heads/main/types/darwin.lua
+curl -L https://github.com/OUIsolutions/LuaCAmalgamator/releases/download/0.001/lua_c_amalgamator_dependencie_not_included.c -o lua_c_amalgamator_dependencie_not_included.c
 curl -L https://github.com/OUIsolutions/Darwin/releases/download/0.013/darwin013.c -o darwin.c
 gcc darwin.c -o darwin.o
