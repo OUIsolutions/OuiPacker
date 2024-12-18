48286e84698f5612bb34d6b4ef5757f62875c307
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-12-18 00:16:39 -0300

    at

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -8,6 +8,6 @@ git clone -b v0.52 https://github.com/OUIsolutions/LuaFluidJson.git
 git clone -b v0.72 https://github.com/OUIsolutions/LuaDoTheWorld.git
 git clone -b V0.003 https://github.com/SamuelHenriqueDeMoraisVitrio/candangoEngine.git
 
-curl -L https://github.com/OUIsolutions/LuaCAmalgamator/releases/download/0.001/lua_c_amalgamator_darwin_import.c  -o lua_c_amalgamator_darwin_import.c
-curl -L https://github.com/OUIsolutions/Darwin/releases/download/0.014/darwin.o -o darwin.o
+curl -L https://github.com/OUIsolutions/LuaCAmalgamator/releases/download/0.002/lua_c_amalgamator_darwin_import.c  -o lua_c_amalgamator_darwin_import.c
+curl -L https://github.com/OUIsolutions/Darwin/releases/download/0.015/darwin.o -o darwin.o
 sudo chmod +x darwin.o
