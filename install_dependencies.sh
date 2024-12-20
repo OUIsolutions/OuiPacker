45dbee9e82737530959c61cb360c5add36721fc5
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-12-20 17:55:23 -0300

    att

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -12,7 +12,7 @@ curl -L https://github.com/OUIsolutions/CTextEngine/releases/download/v2.002/CTe
 curl -L https://github.com/OUIsolutions/LuaCEmbed/releases/download/v0.779/LuaCEmbed.h -o LuaCEmbed.h
 curl -L https://github.com/OUIsolutions/DoTheWorld/releases/download/v7.006/doTheWorld.h -o doTheWorld.h
 curl -L  https://github.com/OUIsolutions/LuaArgv/releases/download/0.02/luargv.lua -o luargv.lua
-curl -L https://github.com/OUIsolutions/LuaSilverChain/releases/download/0.001/silverchain_no_dependecie_included.c -o silverchain_no_dependecie_included.c
+curl -L https://github.com/OUIsolutions/LuaSilverChain/releases/download/0.002/silverchain_no_dependecie_included.c -o silverchain_no_dependecie_included.c
 curl -L https://github.com/OUIsolutions/LuaCAmalgamator/releases/download/0.002/lua_c_amalgamator_dependencie_not_included.c -o lua_c_amalgamator_dependencie_not_included.c
 curl -L https://github.com/OUIsolutions/Darwin/releases/download/0.015/darwin.out -o darwin.out
 sudo chmod +x darwin.out
