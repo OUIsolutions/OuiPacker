e1e53005c1487f1b25afc99074ee8e5cc63df4e7
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-12-20 16:56:17 -0300

    att

--- darwinconf.lua
+++ darwinconf.lua
@@ -1,23 +1,16 @@
 
 
-function is_arg_present(arg_name) 
+function is_arg_present(arg_name)
     for i=1, #arg do
-        if arg[i] == arg_name then 
-            return true 
+        if arg[i] == arg_name then
+            return true
         end
-    end 
+    end
     return false
-end 
+end
 
-
--- eliminantes unwanted prints
---darwin.add_c_code("\n#undef printf\n")
---darwin.add_c_code("#define printf(...) \n")
 darwin.add_c_file("dependencies/lua_c_amalgamator_darwin_import.c")
-darwin.load_lualib_from_c(
-    "luaopen_lua_c_amalgamator",
-    "camalgamator"
-)
+darwin.add_c_file("dependencies/silverchain_no_dependecie_included.c")
 
 darwin.add_c_file("dependencies/LuaDoTheWorld/src/one.c", true, function(import, path)
     -- to make the luacembe not be imported twice
@@ -63,12 +56,20 @@ for i = 1, #types_files do
 end
 darwin.embed_global("PRIVATE_DARWIN_TYPES", types)
 
-
 darwin.load_lualib_from_c("luaopen_lua_c_amalgamator", "camalgamator")
+darwin.load_lualib_from_c("luaopen_lua_silverchain", "silverchain")
+
 darwin.load_lualib_from_c("load_luaDoTheWorld", "dtw")
 darwin.load_lualib_from_c("load_lua", "json")
 
 darwin.load_lualib_from_c("candango_engine_start_point", "candango")
+local lua_argv_content = dtw.load_file("dependencies/luargv.lua")
+darwin.add_lua_code(string.format(
+    "argv = function()\n %s\n end \n",
+    lua_argv_content
+))
+darwin.add_lua_code("argv = argv()")
+
 darwin.add_lua_code("private_oui_packer = {}")
 
 local src_files = dtw.list_files_recursively("src", true)
@@ -81,12 +82,11 @@ darwin.add_lua_code("private_oui_packer.main()")
 darwin.generate_lua_output({ output_name = "debug.lua" })
 darwin.generate_c_executable_output({ output_name = "release/OuiPacker.c" })
 
-if is_arg_present("build_windows") then 
+if is_arg_present("build_windows") then
     os.execute("i686-w64-mingw32-gcc release/OuiPacker.c --static -o  release/OuiPacker.exe")
-end 
+end
 
-if is_arg_present("build_linux") then 
+if is_arg_present("build_linux") then
     os.execute("gcc release/OuiPacker.c --static -o  release/OuiPacker.out")
 
-end 
-
+end
--- install_dependencies.sh
+++ install_dependencies.sh
@@ -7,7 +7,8 @@ rm -rf candangoEngine
 git clone -b v0.52 https://github.com/OUIsolutions/LuaFluidJson.git
 git clone -b v0.72 https://github.com/OUIsolutions/LuaDoTheWorld.git
 git clone -b V0.003 https://github.com/SamuelHenriqueDeMoraisVitrio/candangoEngine.git
-
+curl -L  https://github.com/OUIsolutions/LuaArgv/releases/download/0.02/luargv.lua -o luargv.lua
+curl -L https://github.com/OUIsolutions/LuaSilverChain/releases/download/0.001/silverchain_no_dependecie_included.c -o silverchain_no_dependecie_included.c
 curl -L https://github.com/OUIsolutions/LuaCAmalgamator/releases/download/0.002/lua_c_amalgamator_darwin_import.c  -o lua_c_amalgamator_darwin_import.c
 curl -L https://github.com/OUIsolutions/Darwin/releases/download/0.015/darwin.out -o darwin.out
 sudo chmod +x darwin.out
